9640032445e20480df640d6ee3dbc4d505067583
Alice Jacka <jacka.alice@gmail.com>
2018-12-18 21:25:44 +1100

    update good quality file size

--- inc/home.php
+++ inc/home.php
@@ -6,5 +6,5 @@ Keys can be purcased from the front office. <br>
     <input type="email" name="email" placeholder="Email" style="width: 20em"><br>
     <input type="text" name="key" placeholder="License Key" style="width: 20em"><br>
     <input type="submit" value="Low Quality (800MB)">
-    <input type="submit" name="goodQuality" value="High Quality (2GB)">
+    <input type="submit" name="goodQuality" value="High Quality (4GB)">
 </form>
